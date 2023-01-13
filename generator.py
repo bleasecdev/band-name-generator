8b13eab7b2055a097e963355a7a0966cbad2bb68
Blease Carson <bleasecarson@gmail.com>
2023-01-13 18:49:26 -0500

    Completed program using simple input and print functions in conjunction with variables for storing information.

---
+++ generator.py
@@ -0,0 +1,8 @@
+# Program greeting 
+print("Welcome to the Band Name Generator") 
+# # Collect name of city. 
+city = input("What is the name of the city yopu grew up in?: \n") 
+# # Collect name of pet. 
+pet = input("What is your pet's name?: \n") 
+# # Prints a band name to the console. 
+print("Your bands name could be " + city + " " + pet)
